9e199b651445920c626a3c836c271f1b24a05ad8
Darshan Jain <108680813+darshanjain-entrepreneur@users.noreply.github.com>
2023-01-05 16:41:00 +0530

    Time: 1 ms (75.32%), Space: 45.2 MB (48.77%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
@@ -14,42 +14,19 @@
  * }
  */
 class Solution {
-    
-    public void find(TreeNode root , PriorityQueue<Integer> p , int k){
-        if(root == null){
-            return;
-        }
-        
-        p.add(root.val);
-        
-        find(root.left , p , k);
-        find(root.right , p , k);
-        
-        
-    }
-    
-    public int kthSmallest(TreeNode root, int k) {
-        if(root == null){
-            return -1;
-        }
-        
-        
-        PriorityQueue<Integer> p = new PriorityQueue<>();
-        
-        find( root , p , k);
-        
-    if(p.size() < k){
-        return -1;
-    }
-       
-        
-        int i = 0;
-        int ans = 0;
-        while(i < k){
-         
-            ans = p.poll();
-            i++;
-        }
-        return ans;
-    }
+  int count = 0;
+int result = Integer.MIN_VALUE;
+
+public int kthSmallest(TreeNode root, int k) {
+    traverse(root, k);
+    return result;
+}
+
+public void traverse(TreeNode root, int k) {
+    if(root == null) return;
+    traverse(root.left, k);
+    count ++;
+    if(count == k) result = root.val;
+    traverse(root.right, k);       
+}
 }
