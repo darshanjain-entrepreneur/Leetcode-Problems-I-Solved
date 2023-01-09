1171a1491bbbc4ff3c576852678bfacfb77d0f86
Darshan Jain <108680813+darshanjain-entrepreneur@users.noreply.github.com>
2023-01-09 16:40:13 +0530

    Time: 0 ms (100.00%), Space: 39.5 MB (61.47%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,42 +1,28 @@
 class Solution {
-    
-    public int find(int m , int n , int dp[][]){
-        
-        if( m== 0 && n == 0){
-            
-            return 1;
-        }
-        
-        if(m < 0 || n < 0){
-            return 0;
-        }
-        
-        if(dp[m][n] != -1){
-            return dp[m][n];
-        }
-        
-        
-        int left = find(m , n -1 , dp);
-        int up = find(m -1 , n , dp);
-        
-        return dp[m][n] =  left + up;
-        
-        
-        
-        
-        
-    }
-    
-    
-    
     public int uniquePaths(int m, int n) {
         
-        int dp[][] = new int[m][n];
-        for(int rows[] : dp){
-            Arrays.fill(rows , -1);
-        }
-        
-        return find(m - 1 , n - 1 , dp);
-        
+         int dp[][] = new int[m][n];
+		 for(int i=0; i<m ;i++){
+      for(int j=0; j<n; j++){
+          
+          //base condition
+          if(i==0 && j==0){
+              dp[i][j]=1;
+              continue;
+          }
+          
+          int up=0;
+          int left = 0;
+          
+          if(i>0) 
+            up = dp[i-1][j];
+          if(j>0)
+            left = dp[i][j-1];
+            
+          dp[i][j] = up+left;
+      }
+  }
+  
+  return dp[m-1][n-1];
     }
 }
