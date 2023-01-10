b1e47d05c738d7ab123f8b3737e282d28ff3bf1d
Darshan Jain <108680813+darshanjain-entrepreneur@users.noreply.github.com>
2023-01-10 12:56:45 +0530

    Time: 5 ms (58.58%), Space: 42.4 MB (62.23%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -1,49 +1,38 @@
 class Solution {
-    
-    
-    public int find(List<List<Integer>> triangle , int i , int j , int n , int m , int dp[][]){
-        if(i == n){
-            return triangle.get(i).get(j);
-            
-        }
-    if(dp[i][j] != -1){
-        return dp[i][j];
-    }
-        
-        
-      int down = triangle.get(i).get(j) + find(triangle , i + 1 , j , n , m , dp);
-        int diagonal = triangle.get(i).get(j) + find(triangle , i + 1 , j + 1 , n , m , dp);
-        
-        
-        return  dp[i][j] =   Math.min(down , diagonal);
-        
-        
-        
-    }
-    
-    
-    
-    
-    
     public int minimumTotal(List<List<Integer>> triangle) {
-        
-       
-        
-        
-        
-        
-        
         int n = triangle.size();
         int m = triangle.get(n-1).size();
         
+        
+        
         int dp[][] = new int[n][m];
         
-        for(int rows[] : dp){
-            Arrays.fill(rows , -1);
+        for(int i = 0 ; i  < m; i++){
+            
+            dp[n-1][i] = triangle.get(n-1).get(i);
+            
+            
         }
         
         
-        return find(triangle , 0 , 0 , n - 1 , m - 1 , dp);
+        for(int i = n -2 ; i >= 0; i--){
+            
+            for(int j = i ; j >= 0; j--){
+                
+                int down = triangle.get(i).get(j) + dp[i + 1][j];
+                
+                int up = triangle.get(i).get(j) + dp[i + 1][j + 1];
+                
+                dp[i][j] = Math.min(down , up);
+                
+            }
+            
+            
+            
+        }
+        
+        
+        return dp[0][0];
         
         
     }
