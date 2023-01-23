c829ff0b0bca211c9a3d768615a3d2945c231bca
Darshan Jain <108680813+darshanjain-entrepreneur@users.noreply.github.com>
2023-01-23 21:47:56 +0530

    Time: 1 ms (59.03%), Space: 43.4 MB (27.46%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,17 +1,35 @@
 class Solution {
-    public List<List<Integer>> subsets(int[] nums) {
-    List<List<Integer>> list = new ArrayList<>();
-    Arrays.sort(nums);
-    backtrack(list, new ArrayList<>(), nums, 0);
-    return list;
-}
-
-private void backtrack(List<List<Integer>> list , List<Integer> tempList, int [] nums, int start){
-    list.add(new ArrayList<>(tempList));
-    for(int i = start; i < nums.length; i++){
-        tempList.add(nums[i]);
-        backtrack(list, tempList, nums, i + 1);
-        tempList.remove(tempList.size() - 1);
+    
+    
+    List<List<Integer>> solve = new ArrayList<>();
+    
+    
+    public void finding(int[] nums , List<Integer> ans ,int i){
+        
+        if(i >= nums.length){
+            solve.add(new ArrayList<Integer>(ans));
+            return;
+        }
+        
+        ans.add(nums[i]);
+        finding(nums , ans , i + 1);
+        ans.remove(ans.size() - 1);
+        finding(nums , ans , i+ 1);
+        
+        
+        
     }
-}
+    
+    
+    
+    
+    public List<List<Integer>> subsets(int[] nums) {
+        
+        List<Integer> ans = new ArrayList<>();
+        
+        
+        finding(nums ,ans , 0 );
+        return solve;
+        
+    }
 }
