6a634db800996e040951d709357d001c4aacee56
Darshan Jain <108680813+darshanjain-entrepreneur@users.noreply.github.com>
2023-01-28 13:54:42 +0530

    Time: 13 ms (7.99%), Space: 58.3 MB (8.21%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -6,7 +6,7 @@ class Solution {
         
         int n = nums.length;
         int left[] = new int[n];
-        int right[] = new int[n];
+        
         left[0] = 1;
         
         
@@ -14,18 +14,16 @@ class Solution {
         
             left[i] = left[i-1]*nums[i-1];
         }
-        
-        
-        right[n-1] = 1;
-        for(int i = n-1; i>= 1; i--){
+        int right = 1;
+        for(int i = n-1; i >= 0; i--){
             
-           right[i-1] = right[i]*nums[i];
+            left[i] = left[i] * right;
+            right = right*nums[i];
+            
             
         }
-        
-        for(int i = 0; i < n; i++){
-            left[i] = left[i] *right[i];
-        }
+    
         return left;
+      
     }
 }
