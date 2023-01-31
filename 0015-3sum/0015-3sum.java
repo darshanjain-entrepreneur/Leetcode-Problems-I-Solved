05cb2ab52c3305b44b3c2a4426f42ef61de7d788
Darshan Jain <108680813+darshanjain-entrepreneur@users.noreply.github.com>
2023-01-31 23:52:03 +0530

    Time: 36 ms (62.10%), Space: 49.9 MB (44.72%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,25 +1,39 @@
-
-        
-        class Solution {
+class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
-       List<List<Integer>> res=new LinkedList<>();
-       for(int i=0;i<nums.length-2;i++){
-           if(i==0 || (i>0 && nums[i]!=nums[i-1])){
-           int lo=i+1; int hi=nums.length-1; int sum=0-nums[i];
-           while(lo<hi){
-               if(nums[lo]+nums[hi]==sum){
-                   res.add(Arrays.asList(nums[i],nums[lo],nums[hi]));
-                   while(lo<hi && nums[lo]==nums[lo+1]) lo++;
-                   while(lo<hi && nums[hi]==nums[hi-1]) hi--;
-                   lo++; hi--;
-               }
-               else if(nums[lo]+nums[hi]<sum) lo++;
-               else hi--;
-           }
-       }
-     }
-     return res;
-   
+        List<List<Integer>> res = new LinkedList<>();
+        
+        for(int i =0 ; i < nums.length - 2; i++){
+            
+            if(i == 0 || (i >0 && nums[i] != nums[i-1])){
+                
+                int sum = 0 - nums[i];
+                int low = i + 1;
+                int high = nums.length - 1;
+                while(low < high){
+                    
+                    if(nums[low] + nums[high] == sum){
+                        res.add(Arrays.asList(nums[i] , nums[low] , nums[high]));
+                        while(low < high && nums[low] == nums[low + 1])low++;
+                        while(low < high && nums[high] == nums[high - 1])high--;
+                        high--;
+                        low++;
+                    }else if(nums[low] + nums[high] < sum)low++;
+                    else{
+                        high--;
+                    }
+                    
+                    
+                    
+                }
+                
+                
+            }
+            
+            
+        }
+        
+        
+        return res;
     }
 }
