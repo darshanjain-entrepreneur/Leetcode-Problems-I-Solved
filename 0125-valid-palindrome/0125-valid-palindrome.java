3299748441680515ec5ac078b385dfbb94428e21
Darshan Jain <108680813+darshanjain-entrepreneur@users.noreply.github.com>
2023-02-04 17:49:49 +0530

    Time: 6 ms (63.70%), Space: 41.9 MB (98.00%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,22 +1,33 @@
 class Solution {
     public boolean isPalindrome(String s) {
         
-            String s1= s.trim().toLowerCase();
-        StringBuilder str = new StringBuilder();
-        for(int i=0;i<s1.length();i++){
-            char c = s1.charAt(i);
-            if((c>=48 && c<=57)||(c>=97 && c<=122)){
-                str.append(c);
+          s = s.toLowerCase();
+        StringBuilder b = new StringBuilder();
+        
+        for(int i = 0; i < s.length() ; i++){
+            
+            char ch = s.charAt(i);
+            if(Character.isDigit(ch) || Character.isLetter(ch)){
+                b.append(ch);
             }
+            
+            
         }
-
-        int start= 0;
-        int end= str.length()-1;
-        while(start<=end){
-            if(str.charAt(start)!=str.charAt(end)) return false;
-            start++; end--;
+        
+        
+        int start = 0;
+        int end = b.length()-1;
+        while(start <= end){
+            if(b.charAt(start) != b.charAt(end)){
+                return false;
+            }
+            start++;
+            end--;
+            
         }
-
+        
+        
         return true;
+        
     }
 }
