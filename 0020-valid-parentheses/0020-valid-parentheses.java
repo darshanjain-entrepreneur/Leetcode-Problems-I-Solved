6da4c5cf01b57c5b08a701c5af664c12c88c434a
Darshan Jain <108680813+darshanjain-entrepreneur@users.noreply.github.com>
2023-02-04 19:00:37 +0530

    Time: 1 ms (99.26%), Space: 39.9 MB (96.84%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,40 @@
+class Solution {
+    public boolean isValid(String s) {
+        
+        
+        
+        Stack<Character> st = new Stack<>();
+        
+        
+        for(int i = 0; i < s.length(); i++){
+            
+            char ch = s.charAt(i);
+            if(ch == '(' || ch== '[' || ch=='{'){
+                
+                st.add(ch);
+                
+            }else{
+                if(  st.isEmpty() == false &&   ch == ')' && st.peek() == '('){
+                    st.pop();
+                }else if(  st.isEmpty() == false && ch == '}' && st.peek() == '{'){
+                    st.pop();
+                    
+                }else if(  st.isEmpty() == false &&  ch == ']' && st.peek() == '['){
+                    st.pop();
+                }else{
+                    return false;
+                }
+               
+            }
+            
+           
+        }
+        
+    if(st.size() == 0){
+        return true;
+    }else{
+        return false;
+    }
+        
+    }
+}
