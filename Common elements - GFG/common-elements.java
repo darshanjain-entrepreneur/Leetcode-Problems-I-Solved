c57c52c2c2f003348155e080f3fa6bfc8e083e6b
Darshan Jain <108680813+darshanjain-entrepreneur@users.noreply.github.com>
2023-02-14 12:42:45 +0530

    Added solution - LeetHub

---
+++ Common elements - GFG/common-elements.java
@@ -0,0 +1,123 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+
+public class GFG
+{
+    public static void main(String args[]) 
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) 
+        {
+            int n1 = sc.nextInt();
+            int n2 = sc.nextInt();
+            int n3 = sc.nextInt();
+            
+            int A[] = new int[n1];
+            int B[] = new int[n2];
+            int C[] = new int[n3];
+            
+            for (int i = 0;i < n1;i++)
+            {
+                A[i] = sc.nextInt();
+            }
+            for (int i = 0;i < n2;i++)
+            {
+                B[i] = sc.nextInt();
+            }
+            for (int i = 0;i < n3;i++)
+            {
+                C[i] = sc.nextInt();
+            }
+            
+            Solution sol = new Solution();
+            ArrayList<Integer> res = sol.commonElements(A, B, C, n1, n2, n3);
+            if (res.size() == 0)
+            {
+                System.out.print(-1);
+            }
+            else 
+            {
+                for (int i = 0;i < res.size();i++)
+                {
+                    System.out.print(res.get(i) + " ");
+                }    
+            }
+            System.out.println();
+        }
+    }
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    ArrayList<Integer> commonElements(int A[], int B[], int C[], int n1, int n2, int n3) 
+    {
+     
+     HashMap<Integer , Integer> map = new HashMap<>();
+     ArrayList<Integer> answer = new ArrayList<Integer>();
+     
+     for(int i = 0; i < n1; i++){
+         
+       if(!map.containsKey(A[i])){
+           
+           map.put(A[i] , 1);
+           
+       }
+         
+         
+         
+     }
+     
+     
+     
+       for(int i = 0; i < n2; i++){
+           
+           if(map.containsKey(B[i])){
+               
+               map.put(B[i] , 2);
+               
+               
+               
+           }
+           
+           
+           
+           
+       }
+       
+   
+   for(int i = 0; i < n3; i++){
+       
+       if(map.containsKey(C[i])){
+           
+           if(map.get(C[i]) == 2){
+               
+               map.put(C[i] , 3);
+               answer.add(C[i]);
+               
+           }
+           
+           
+           
+       }
+          
+       
+       
+   }
+     
+    
+    
+ return answer;
+  
+    
+       
+    }
+}
