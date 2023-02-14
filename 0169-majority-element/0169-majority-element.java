e6856b89b83f975bc075725f5348672ab4f4a9ec
Darshan Jain <108680813+darshanjain-entrepreneur@users.noreply.github.com>
2023-02-14 19:36:13 +0530

    Time: 1 ms (99.93%), Space: 46.3 MB (46.92%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,33 +1,26 @@
 class Solution {
     public int majorityElement(int[] nums) {
         
-         HashMap<Integer , Integer> ans = new HashMap<>();
-       
-       if(nums.length == 1){
-           return nums[0];
-       }
-       
-       
-       
-       for(int i = 0; i < nums.length; i++){
-           
-           if(ans.containsKey(nums[i])){
-               
-               ans.put(nums[i] , ans.get(nums[i]) + 1);
-              if(ans.get(nums[i]) > nums.length/2){
-                  return nums[i];
-              }
-               
-           }else{
-               ans.put(nums[i] , 1);
-           }
-           
-           
-           
-           
-       }
-       return -1;
+   // Moore algorithm
         
         
+        int count = 0;
+        int candidate = 0;
+        
+        for(int num : nums){
+            
+            if(count == 0){
+                candidate = num;
+                
+            }
+            if(num == candidate){
+                count++;
+            }else{
+                count--;
+            }
+            
+        }
+        
+        return candidate;
     }
 }
