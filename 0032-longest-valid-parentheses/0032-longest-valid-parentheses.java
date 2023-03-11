062edbd64732885a3bc84f4315f2f9287132f8a7
Darshan Jain <108680813+darshanjain-entrepreneur@users.noreply.github.com>
2023-03-11 23:47:08 +0530

    Time: 2 ms (82.70%), Space: 42.5 MB (28.63%) - LeetHub

--- 0032-longest-valid-parentheses/0032-longest-valid-parentheses.java
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.java
@@ -9,35 +9,69 @@ class Solution {
         int ans = 0;
         
         
-        Stack<Integer> it = new Stack<>();
-        it.add(-1);
+       int left;
+        int right;
+        left = right = 0;
         
-        for(int i = 0; i < s.length(); i++){
+        for(int i = 0 ; i< s.length(); i++){
             
             char ch = s.charAt(i);
             
-            
             if(ch == '('){
-              
-                it.add(i);
+                
+                left++;
+                
             }else{
-             
-                it.pop();
-                if(it.isEmpty()){
-                    
-                    it.add(i);
-                    
-                }else{
-                    ans = Math.max(ans , i - it.peek());
-                }
+                right++;
+                
+              
+                
                 
                 
             }
+              if(right == left){
+                    ans = Math.max(ans , 2*right);
+                }else if(right > left){
+                    
+                    right = left = 0;
+                }
             
             
             
         }
+        
+        
+        
+        right = left = 0;
+        for(int i = s.length() - 1; i >= 0; i--){
+            
+            char ch = s.charAt(i);
+            if(ch == '('){
+                
+                left++;
+            }else{
+                
+                right++;
+                
+              
+            }
+            
+              if(right==left){
+                    ans = Math.max(ans , 2*right);
+                }else if(left > right){
+                    
+                    left = right = 0;
+                }
+            
+            
+            
+            
+        }
+        
+        
+        
         return ans;
+        
         
     }
 }
