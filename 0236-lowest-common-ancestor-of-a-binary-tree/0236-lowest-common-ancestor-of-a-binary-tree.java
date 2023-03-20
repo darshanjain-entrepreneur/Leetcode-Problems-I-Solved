feff77e6b93d3eb81c58554f4ff5b12b9afcb029
Darshan Jain <108680813+darshanjain-entrepreneur@users.noreply.github.com>
2023-03-20 13:00:36 +0530

    Time: 11 ms (13.64%), Space: 44.5 MB (16.38%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
@@ -8,36 +8,68 @@
  * }
  */
 class Solution {
+    
+    public boolean find(TreeNode root ,int target ,ArrayList<TreeNode> ans ){
+        
+        if(root == null){
+            return false;
+        }
+        
+        ans.add(root);
+        if(root.val == target){
+            return true;
+        }
+        
+        
+        if(find(root.left , target , ans) || find(root.right , target , ans)){
+            
+            return true;
+            
+        }
+        
+        ans.remove(ans.size() - 1);
+        return false;
+        
+    }
+    
+    
+    
+    
+    
+    
+    
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         
-        return find(root , p , q);
         
+        ArrayList<TreeNode> p1 = new ArrayList<>();
+         ArrayList<TreeNode> q1 = new ArrayList<>();
         
-    }
-    
-    
-    public TreeNode find(TreeNode root, TreeNode p, TreeNode q){
+        TreeNode temp1 =root;
+        TreeNode temp2 = root;
         
-        if(root == null || root == p || root == q){
-            return root;
+        find(temp1 , p.val , p1);
+        find(temp2 , q.val , q1);
+        
+        int n = p1.size();
+        int m = q1.size();
+        
+        int i = 0;
+        int j = 0;
+      TreeNode a  = p;
+        
+        while( i< n && j < m){
+            
+            if(p1.get(i) == q1.get(j)){
+                a = p1.get(i);
+            }else{
+                break;
+            }
+            i++;
+            j++;
+            
         }
         
-        
-        TreeNode left = find(root.left , p , q);
-        TreeNode right = find(root.right , p , q);
-        
-        if(left == null){
-            
-            return right;
-        }else if(right == null){
-            
-            return left;
-        }else{
-            return root;
-        }
-        
-        
+        return a;
         
     }
-    
 }
