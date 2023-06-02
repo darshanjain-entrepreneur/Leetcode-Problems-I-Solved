cc00686194262f2563ebdfbeda68f3fdb45442eb
Darshan Jain <108680813+darshanjain-entrepreneur@users.noreply.github.com>
2023-06-02 21:28:04 +0530

    Time: 0 ms (100.00%), Space: 40.9 MB (60.80%) - LeetHub

--- 0289-game-of-life/0289-game-of-life.java
+++ 0289-game-of-life/0289-game-of-life.java
@@ -5,7 +5,7 @@ class Solution {
         int n = board.length;
         int m = board[0].length;
         
-        int ans[][] = new int[n][m];
+    
         
         int drow[] = {-1 , 0 , 1 , 0 ,1 , -1 , 1 , -1};
         int dcol[] = {0 , 1 , 0 ,-1 , 1 , -1 ,-1 , 1 };
@@ -20,7 +20,7 @@ class Solution {
                     int nrow = i + drow[k];
                     int ncol = j + dcol[k];
                     
-                    if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && board[nrow][ncol] == 1){
+                    if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && (board[nrow][ncol] == 1 || board[nrow][ncol] == 2 ) ){
                         count++;
                     }
                     
@@ -29,13 +29,13 @@ class Solution {
                 
                 if(board[i][j] == 1){
                     
-                    if(count == 2 || count == 3){
-                        ans[i][j] = 1;
+                    if(count < 2 || count >3){
+                      board[i][j] = 2;
                     }
                 }else{
                     
                     if(count == 3){
-                        ans[i][j] = 1;
+                        board[i][j] = 3;
                     }
                 }
                 
@@ -45,11 +45,13 @@ class Solution {
         
         
         
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
-                board[i][j] = ans[i][j];
-            }
-        }
+      for(int i = 0; i < n; i++){
+          for(int j = 0;j < m; j++){
+              if(board[i][j] == 2 || board[i][j] == 3){
+                  board[i][j] = board[i][j]%2;
+              }
+          }
+      }
         
         
     }
