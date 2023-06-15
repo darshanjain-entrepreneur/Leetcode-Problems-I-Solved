64c52164defdc303e9f55c74d7d241d8b050065d
Darshan Jain <108680813+darshanjain-entrepreneur@users.noreply.github.com>
2023-06-15 22:48:31 +0530

    Time: 0 ms (100.00%), Space: 39.6 MB (44.76%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
@@ -3,9 +3,25 @@ class Solution {
         
         
         int dp[] = new int[n+1];
-        Arrays.fill(dp , -1);
+        dp[0] = 0;
+       if(n >= 1){
+           
+           dp[1] = 1;
+       }
+        if(n>=2){
+            dp[2] = 1;
+        }
+      
+        for(int i = 3; i <= n; i++){
+            
+            
+            dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
+            
+            
+        }
         
-        return find(n , dp);
+        return dp[n];
+        
     }
     
     
