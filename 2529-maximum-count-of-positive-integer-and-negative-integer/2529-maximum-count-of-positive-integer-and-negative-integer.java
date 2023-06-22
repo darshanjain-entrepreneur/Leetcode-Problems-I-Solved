e819f53a8702bacfc7337d4d6785ba2e9eb1d0a4
Darshan Jain <108680813+darshanjain-entrepreneur@users.noreply.github.com>
2023-06-23 00:46:10 +0530

    Time: 2 ms (16.85%), Space: 44.5 MB (7.44%) - LeetHub

--- 2529-maximum-count-of-positive-integer-and-negative-integer/2529-maximum-count-of-positive-integer-and-negative-integer.java
+++ 2529-maximum-count-of-positive-integer-and-negative-integer/2529-maximum-count-of-positive-integer-and-negative-integer.java
@@ -1,25 +1,89 @@
 class Solution {
     public int maximumCount(int[] nums) {
         
+        System.out.println(nums.length);
         int pos = 0;
         int neg = 0;
         
-        for(int i : nums){
+        
+        int start = 0;
+        int count = 0;
+        int end = nums.length-1;
+        
+        while(start <= end){
             
-            if(i > 0){
-                pos++;
-            }else if(i < 0){
+            int mid = start + (end-start)/2;
+            
+            if(nums[mid] > 0 && mid-1 >= 0 && nums[mid-1] <= 0){
                 
-                neg++;
+                pos = mid;
+                count=1;
+                break;
+                
+                
+            }else{
+                
+                if(nums[mid] <= 0){
+                    start = mid+1;
+                }else{
+                    end = mid-1;
+                    
+                }
+                
+                
             }
-            
-            
-            
             
             
             
         }
         
-        return Math.max(pos ,neg);
+     if(count == 0){
+         
+         pos = start;
+       
+     }
+        
+        count = 0;
+        
+        start = 0;
+        end = nums.length-1;
+        
+            while(start <= end){
+            
+            int mid = start + (end-start)/2;
+            
+            if(nums[mid] < 0 && mid+1 < nums.length && nums[mid+1] >= 0){
+                
+                neg = mid;
+                count=1;
+                break;
+                
+                
+            }else{
+                
+                if(nums[mid] >= 0){
+                    end = mid-1;
+                }else{
+                  start = mid+1;
+                    
+                }
+                
+                
+            }
+            
+            
+            
+        }
+        
+        if(count == 0){
+            
+           neg =end;
+            
+        }
+        
+        
+        return Math.max(nums.length-pos , neg+1);
+        
+        
     }
 }
