7aa27c8a2580241a92f4a5ed2e188b6b798c688f
Darshan Jain <108680813+darshanjain-entrepreneur@users.noreply.github.com>
2023-06-23 16:29:44 +0530

    Time: 1 ms (82.66%), Space: 39.2 MB (86.20%) - LeetHub

---
+++ 1952-three-divisors/1952-three-divisors.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isThree(int n) {
+        
+        
+        int count = 1;
+        
+        
+        for(int i =1; i <= n/2; i++){
+            
+            if(n%i == 0){
+                
+                count++;
+            }
+        }
+        
+        
+       return count == 3; 
+    }
+}
