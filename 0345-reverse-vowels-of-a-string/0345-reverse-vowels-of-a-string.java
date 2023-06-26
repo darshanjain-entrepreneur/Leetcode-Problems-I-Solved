e1882b8a20bfdfdf6f6333507c1114c7a57c125a
Darshan Jain <108680813+darshanjain-entrepreneur@users.noreply.github.com>
2023-06-26 23:15:30 +0530

    Time: 3 ms (97.76%), Space: 43.6 MB (79.21%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
@@ -1,61 +1,63 @@
 class Solution {
     public String reverseVowels(String s) {
         
-        StringBuilder b = new StringBuilder();
+        StringBuilder b = new StringBuilder(s);
+        
+        int start = 0;
+        int end = s.length()-1;
+        int n = s.length();
         
         
-        for(int i = 0; i < s.length(); i++){
+        while(start <= end){
             
-            char ch = s.charAt(i);
+            char ch1 = b.charAt(start);
+            char ch2 = b.charAt(end);
             
-            if(ch == 'a' || ch=='e' || ch=='i' || ch=='o' || ch=='u'|| ch=='A'|| ch=='I'||ch=='E'||ch=='O'||ch=='U'){
+    while(start < n && ch1 != 'a'&& ch1 !='e'&& ch1!='i'&&ch1!='o'&&ch1!='u'&&ch1!='A'&&ch1!='E'&&ch1!='I'&&ch1!='O'&&ch1!='U'){
+        
+        
+        start++;
+        if(start < n){
+                 ch1 = b.charAt(start);
+        }
+   
+        
+        
+    }        
+    
+     while(end >= 0 && ch2 != 'a'&& ch2!='e'&& ch2!='i'&&ch2!='o'&&ch2!='u'&&ch2!='A'&&ch2!='E'&&ch2!='I'&&ch2!='O'&&ch2!='U'){
+        
+        
+       end--;
+    
+        if(end >= 0){
+                 ch2 = b.charAt(end);
+        }
+   
+        
+        
+        
+    } 
+    
+            if(start<=end){
                 
+                char c = b.charAt(start);
+                char d = b.charAt(end);
                 
-                b.append(ch);
-                
+                b.setCharAt(start , d);
+                b.setCharAt(end , c);
+                start++;
+                end--;
                 
             }
+
+           
             
             
             
         }
         
-        
-        if(b.length() == 0){
-            return s;
-        }
-        b.reverse();
-        System.out.print(b);
-        
-        StringBuilder c = new StringBuilder(s);
-        int k = 0;
-        
-        for(int i = 0; i < s.length(); i++){
-            
-                char ch = s.charAt(i);
-            if(ch == 'a' || ch=='e' || ch=='i' || ch=='o' || ch=='u'|| ch=='A'|| ch=='I'||ch=='E'||ch=='O'||ch=='U' ){
-                
-                
-              c.setCharAt(i ,b.charAt(k) );
-                System.out.println(k);
-                k++;
-                  
-                  
-              
-                
-                
-            }
-            
-            
-            
-            
-        }
-        
-        
-        
-        
-        return c.toString();
-        
+        return b.toString();
         
     }
 }
