58970cfe0df8d59b81bfa5662a26e542d680ed6d
Darshan Jain <108680813+darshanjain-entrepreneur@users.noreply.github.com>
2023-07-10 00:26:29 +0530

    Time: 0 ms (100.00%), Space: 39.3 MB (61.98%) - LeetHub

---
+++ 0367-valid-perfect-square/0367-valid-perfect-square.java
@@ -0,0 +1,45 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        
+        if(num ==1){
+            return true;
+        }
+        
+      long start = 0;
+      long end = num/2;
+        
+        while(start <= end){
+            
+           long mid = start+ (end-start)/2;
+      
+               if(mid*mid <= 0){
+                   end = mid-1;
+                   System.out.println(mid*mid);
+               }else{
+                   if(mid*mid == num){
+                       return true;
+                   }else{
+                       
+                       if(mid*mid > num){
+                           end=mid-1;
+                       }else{
+                           start = mid+1;
+                       }
+                   }
+                   
+               }
+            
+            
+                
+            
+                 
+        }
+        
+        
+        
+        
+        return false;
+        
+        
+    }
+}
