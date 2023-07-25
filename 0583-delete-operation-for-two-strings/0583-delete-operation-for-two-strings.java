762e5e005468e7586623bac7f8426efc90a6cc2f
Darshan Jain <108680813+darshanjain-entrepreneur@users.noreply.github.com>
2023-07-25 17:32:07 +0530

    Time: 11 ms (39.06%), Space: 43.9 MB (23.12%) - LeetHub

--- 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.java
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.java
@@ -2,44 +2,40 @@ class Solution {
     public int minDistance(String word1, String word2) {
         
         
-        int dp[][] = new int[word1.length()][word2.length()];
+        int dp[][] = new int[word1.length()+1][word2.length()+1];
         
-        for(int rows[] : dp){
+      for(int i = 1; i <=word1.length(); i++){
+          
+          for(int j = 1; j <= word2.length(); j++){
+              
+           if(word1.charAt(i-1) == word2.charAt(j-1)){
             
-            Arrays.fill(rows , -1);
-        }
+             dp[i][j] =  1 + dp[i-1][j-1];
+        }else{
+            dp[i][j] =  Math.max(dp[i-1][j],dp[i][j-1]);
+        }    
+              
+              
+              
+          }
+          
+          
+      }
         
         
-        int ans = find(word1.length()-1 , word2.length()-1 ,word1 , word2 , dp);
+        
+        
+        int ans = dp[word1.length()][word2.length()];
         
         return word1.length()- ans + word2.length()-ans;
         
         
-        
+  
     }
     
-    public int find(int i , int j , String word1 , String word2 , int dp[][]){
-        
-        if(i < 0 || j < 0){
-            return 0;
-        }
-        
-        if(dp[i][j]!= -1){
-            return dp[i][j];
-        }
+ 
         
         
-        if(word1.charAt(i) == word2.charAt(j)){
-            
-            return dp[i][j] =  1 + find(i-1 , j-1 , word1 , word2 , dp);
-        }else{
-            
-            return dp[i][j] =  Math.max(find(i-1 , j , word1 , word2 , dp),find(i , j-1 , word1 , word2 , dp));
-        }
-        
-        
-        
-        
-    }
+    
     
 }
