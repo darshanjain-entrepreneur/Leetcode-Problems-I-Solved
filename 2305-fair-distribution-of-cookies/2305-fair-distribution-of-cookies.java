f268fc9a95d91926b5e71ac2bebb7272fa8b65f1
Darshan Jain <108680813+darshanjain-entrepreneur@users.noreply.github.com>
2023-08-01 14:48:10 +0530

    Time: 355 ms (49.58%), Space: 39.7 MB (88.32%) - LeetHub

---
+++ 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.java
@@ -0,0 +1,53 @@
+class Solution {
+    
+    int min = (int)1e9;
+    
+    
+    
+    public int distributeCookies(int[] cookies, int k) {
+ 
+        int arr[] = new int[k];
+            findmax(0 , k , cookies , arr);
+        
+        return min;
+        
+        
+        
+        
+    }
+    
+    public void findmax(int i , int k , int[] cookies , int[] arr){
+        
+       
+        if(i >= cookies.length){
+            int max = -1;
+           for(int j = 0; j < arr.length; j++){
+               
+              max =  Math.max(max , arr[j]);
+               
+           }
+            
+            min = Math.min(max , min);
+            
+            return;
+        }
+        
+        
+        for(int j = 0; j < k; j++){
+            
+           arr[j] = arr[j] + cookies[i];
+            
+            
+             
+              findmax(i+1 , k , cookies , arr);
+            
+            arr[j] = arr[j] - cookies[i];
+            
+            
+        }
+        
+        
+        
+        
+    }
+}
