1a8b99986c0f18a27626967354bfbcbdc62faf8a
Darshan Jain <108680813+darshanjain-entrepreneur@users.noreply.github.com>
2023-08-03 13:56:47 +0530

    Time: 6 ms (11.76%), Space: 41.1 MB (76.76%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,46 +1,78 @@
 class Solution {
     public List<String> letterCombinations(String digits) {
         
+        if(digits.length() == 0){
+            
+            return new ArrayList<String>();
+        }
         
-        String digitarray[] = {"" , "" , "abc" , "def" , "ghi" , "jkl" , "mno" , "pqrs" , "tuv" , "wxyz"};
+      // storing list of strings  
+    List<String> PossibleOrder = new ArrayList<>();
         
-        List<String> ans = new ArrayList<>();
         
-        find(0 , digits , ans , digitarray , "");
+    String characterStoring[]  = new String[10];
         
-        return ans;
+     characterStoring[2]  = "abc";
+        characterStoring[3] = "def";
+        characterStoring[4] = "ghi";
+        characterStoring[5] = "jkl";
+        characterStoring[6] = "mno";
+        characterStoring[7] = "pqrs";
+        characterStoring[8] = "tuv";
+        characterStoring[9] = "wxyz";
+        
+        
+        
+        
+        
+        // function to find all strings
+        findPossibleStrings(0 , digits , digits.length() , "" , characterStoring , PossibleOrder);
+        
+     
+        return PossibleOrder;
+        
         
     }
     
-    public void find(int index , String digits , List<String> ans ,  String digitarray[] , String p ){
-        
-        if(index >= digits.length()){
-            
-            if(p == ""){
-                
-            }else{
-                 ans.add(p);
-            }
-           
-            return;
-        }
-        
-        
-    String word = digitarray[digits.charAt(index) - '0'];
-        
-        for(int i = 0; i< word.length(); i++){
-            
-            find(index +1 , digits , ans , digitarray , p + word.charAt(i));
-            
-            
-            
-        }
+public void findPossibleStrings(int i , String digits , int n , String p , String[] arr, List<String> PossibleOrder){
     
+    
+    
+    if(p.length() ==n){
         
+        if(PossibleOrder.contains(p) == false){
+            
+            PossibleOrder.add(p);
+        }
         
+        return;
+    }
+    
+    
+    if(i >= n){
+        return;
+    }
+    
+    int a = (int)(digits.charAt(i) - '0');
+    String b = arr[a];
+    
+     char c[] = b.toCharArray();
+    
+    for(char d : c){
         
+        findPossibleStrings(i+1 , digits , n , p + d , arr , PossibleOrder);
         
         
         
     }
+    
+    
+    
+    
+    
+    
+    
+}
+    
+    
 }
