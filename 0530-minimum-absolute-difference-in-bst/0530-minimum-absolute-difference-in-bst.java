55b675ed3a02737ea456b91337dabd27841a2600
Darshan Jain <108680813+darshanjain-entrepreneur@users.noreply.github.com>
2023-08-03 22:26:07 +0530

    Time: 1 ms (62.97%), Space: 43.4 MB (56.71%) - LeetHub

---
+++ 0530-minimum-absolute-difference-in-bst/0530-minimum-absolute-difference-in-bst.java
@@ -0,0 +1,44 @@
+
+class Solution {
+    public int getMinimumDifference(TreeNode root) {
+        
+         List<Integer> ll = new ArrayList<>();
+        
+        find(root , ll);
+        
+      
+        
+        int min = (int)1e9;
+        
+        for(int i = 0; i < ll.size()-1; i++ ){
+            
+            min = Math.min(min , ll.get(i+1) - ll.get(i));
+            
+            
+            
+            
+        }
+    
+        
+        return min;
+    }
+    
+    public void find(TreeNode root , List<Integer> ll ){
+        
+        
+        if(root == null){
+            return;
+        }
+        
+        
+      
+        
+        find(root.left , ll);
+         ll.add(root.val);
+        find(root.right , ll);
+        
+        
+        
+        
+    }
+}
