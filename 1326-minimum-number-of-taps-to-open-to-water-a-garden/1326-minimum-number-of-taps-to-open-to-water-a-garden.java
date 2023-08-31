049c09a4657dbe4a57f070bf3292ef036dbe76c9
Darshan Jain <108680813+darshanjain-entrepreneur@users.noreply.github.com>
2023-08-31 16:17:24 +0530

    Time: 9 ms (49.39%), Space: 43.9 MB (81.78%) - LeetHub

--- 1326-minimum-number-of-taps-to-open-to-water-a-garden/1326-minimum-number-of-taps-to-open-to-water-a-garden.java
+++ 1326-minimum-number-of-taps-to-open-to-water-a-garden/1326-minimum-number-of-taps-to-open-to-water-a-garden.java
@@ -1,28 +1,32 @@
 class Solution {
     public int minTaps(int n, int[] ranges) {
-        int[] intervals = new int[n];
-        Arrays.fill(intervals, -1);
-        for (int i = 0; i < ranges.length; i++) {
-            if (ranges[i] == 0) continue;
-            int left = i - ranges[i] >= 0 ? i - ranges[i] : 0;
-            int right = i + ranges[i];
-            intervals[left] = Math.max(intervals[left], right);
-        }
-        if (intervals[0] == -1) return -1;
-        int longest = intervals[0];
-        int count = 1;
-        int i = 0;
-        while (longest < n) {
-            int temp = Integer.MIN_VALUE;
-            for (; i <= longest; i++) {
-                int val = intervals[i];
-                if (val == -1) continue;
-                temp = Math.max(temp, val);
+        
+    int max = 0;
+        int min = 0;
+        int tap = 0;
+        
+        while(max < n){
+            
+            for(int i = 0; i < ranges.length; i++){
+                
+                
+                if(min >= (i-ranges[i]) && (i+ranges[i]) > max ){
+                    
+                    max = i+ranges[i];
+                    
+                }
             }
-            if (temp <= longest) return -1;
-            longest = temp;
-            count++;
+            
+            if(min == max){
+                return -1;
+            }
+            min = max;
+            tap++;
+            
         }
-        return count;
+        
+        
+        
+        return tap;
     }
 }
