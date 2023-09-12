1c9890c1f123290419958ee25e1cac7a2202a822
Darshan Jain <108680813+darshanjain-entrepreneur@users.noreply.github.com>
2023-09-12 14:02:17 +0530

    Time: 7 ms (98.85%), Space: 44.4 MB (78.89%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.java
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.java
@@ -1,76 +1,22 @@
 class Solution {
     public int minDeletions(String s) {
+          int[] charCount = new int[26];
         
-        
-        
-
-
- char arr[] = s.toCharArray();
-        
-        
-
- Arrays.sort(arr);
-        String b = new String(arr);
-
-      
-        int freq[] = new int[26];
-        
-        for(int i = 0; i < b.length(); i++){
-            
-            int a = (int)(b.charAt(i) - 'a');
-            freq[a]++;
+        for(char ch : s.toCharArray()){
+            charCount[ ch - 'a'] ++;
         }
         
+        HashSet<Integer> set = new HashSet<>();
+        int deletion = 0;
         
-    HashMap<Integer , Integer> map = new HashMap<>();
-           
-   for(int i : freq){
-       
-       if(i > 0){
-           map.put(i, map.getOrDefault(i , 0) + 1);
-       }
-       
-       
-   }
-        
-     
-     int delete = 0;
-        
-      int i = 0;
-        
-        while(i < b.length()){
-            
-            char c = b.charAt(i);
-            while(i < b.length() && b.charAt(i) == c){
-                
-                i++;
+        for(int val : charCount){
+            while(val !=0 && set.contains(val)){
+                val--;
+                deletion++;
             }
-            
-            int a = (int)(c-'a');
-            int frequency = freq[a];
-           if(map.get(frequency) > 1){
-               delete++;
-               map.put(frequency , map.get(frequency) - 1);
-               for(int j = frequency-1; j > 0; j--){
-                   
-                   if(!map.containsKey(j)){
-                       map.put(j , 1);
-                       break;
-                   }else{
-                       delete++;
-                   }
-                   
-               }
-            
-               
-               
-           }
-            
-            
+            set.add(val);          
         }
-        
-        
-        
-        return delete;
+       
+        return deletion;
     }
 }
