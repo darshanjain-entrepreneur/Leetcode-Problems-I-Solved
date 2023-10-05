d4bf3ebf629c95aac96205a15b52f0a88f00bcdc
Darshan Jain <108680813+darshanjain-entrepreneur@users.noreply.github.com>
2023-10-05 21:18:08 +0530

    Time: 8 ms (50.50%), Space: 47 MB (16.25%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -2,23 +2,41 @@ class Solution {
     public List<Integer> majorityElement(int[] nums) {
         
         
-        HashMap<Integer , Integer> map = new HashMap<>();
+Arrays.sort(nums);
+        List<Integer> ans = new ArrayList<>();
+        
+        
         int n = nums.length/3;
-        
-       List<Integer> ans = new ArrayList<>();
-        
-        for(int i : nums){
-            
-            map.put(i , map.getOrDefault(i , 0)+1);
+        int count = 1;
+        if(n == 0){
+            ans.add(nums[0]);
         }
         
-        for(int i : map.keySet()){
+        for(int i = 1; i < nums.length; i++){
             
-            if(map.get(i) > n){
-                ans.add(i);
+            if(nums[i] == nums[i-1]){
+                count++;
+               if(ans.size() == 0 && count > n){
+                   ans.add(nums[i]);
+               }else if(ans.size() > 0 && ans.get(ans.size()-1) != nums[i] && count > n){
+                   ans.add(nums[i]);
+               }
+                
+            }else{
+                
+                count = 1;
+                  if(ans.size() == 0 && count > n){
+                   ans.add(nums[i]);
+               }else if(ans.size() > 0 && ans.get(ans.size()-1) != nums[i] && count > n){
+                   ans.add(nums[i]);
+               }
+                
             }
             
+            
+            
         }
+        
         
         return ans;
     }
