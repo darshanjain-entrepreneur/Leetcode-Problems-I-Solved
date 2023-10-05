92e36583b2728cdfb82ced02c62ed54ee0bdee0a
Darshan Jain <108680813+darshanjain-entrepreneur@users.noreply.github.com>
2023-10-05 21:58:05 +0530

    Time: 1 ms (99.94%), Space: 46.8 MB (38.25%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -2,42 +2,57 @@ class Solution {
     public List<Integer> majorityElement(int[] nums) {
         
         
-Arrays.sort(nums);
         List<Integer> ans = new ArrayList<>();
         
+        int maj1 = 0;
+        int maj2 = 0;
+        int count1 = 0;
+        int count2 = 0;
         
-        int n = nums.length/3;
-        int count = 1;
-        if(n == 0){
-            ans.add(nums[0]);
-        }
-        
-        for(int i = 1; i < nums.length; i++){
+        for(int i : nums){
             
-            if(nums[i] == nums[i-1]){
-                count++;
-               if(ans.size() == 0 && count > n){
-                   ans.add(nums[i]);
-               }else if(ans.size() > 0 && ans.get(ans.size()-1) != nums[i] && count > n){
-                   ans.add(nums[i]);
-               }
-                
+            if(i == maj1){
+                count1++;
+            }else if(i == maj2){
+                count2++;
+            }else if(count1 == 0){
+                maj1 = i;
+                count1 = 1;
+            }else if(count2 == 0){
+                maj2 = i;
+                count2 = 1;
             }else{
                 
-                count = 1;
-                  if(ans.size() == 0 && count > n){
-                   ans.add(nums[i]);
-               }else if(ans.size() > 0 && ans.get(ans.size()-1) != nums[i] && count > n){
-                   ans.add(nums[i]);
-               }
-                
+                count1--;
+                count2--;
             }
             
             
+             }
+        
+        
+        int freq1 = 0;
+        int freq2 = 0;
+        
+        for(int i : nums){
+            
+            if(i == maj1){
+                freq1++;
+            }else if(i == maj2){
+                freq2++;
+            }
             
         }
         
+        if(freq1 > (nums.length/3)){
+            ans.add(maj1);
+        }
+        
+        if(freq2 > (nums.length/3)){
+            ans.add(maj2);
+        }
         
         return ans;
+        
     }
 }
