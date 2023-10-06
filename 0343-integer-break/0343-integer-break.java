afba48fc88e121f5fa6d0efdaf7263e572b42160
Darshan Jain <108680813+darshanjain-entrepreneur@users.noreply.github.com>
2023-10-06 14:52:41 +0530

    Time: 0 ms (100.00%), Space: 38.9 MB (85.29%) - LeetHub

--- 0343-integer-break/0343-integer-break.java
+++ 0343-integer-break/0343-integer-break.java
@@ -1,30 +1,39 @@
 class Solution {
     public int integerBreak(int n) {
         
-        if(n==2 || n==3){
-            return n-1;
-        }
         
-        int a = n/3;
-        int b = (int)(Math.pow(3 , a));
-        int c = n-(3*a);
-        c = c/2;
-        int d = (int)(Math.pow(2 , c));
-        int ans = b*d;
+        int dp[] = new int[n+1];
+        Arrays.fill(dp , -1);
         
-    
-        while(a >= 0){
-          
-         b = (int)(Math.pow(3 , a));
-       c = n-(3*a);
-        c = c/2;
-       d = (int)(Math.pow(2 , c));
-      ans = Math.max(ans , b*d);
-            a--;
-        }
-        
-        
-        
-       return ans;
+        return find(n , dp);
     }
+    
+    
+    public int find(int n , int dp[]){
+        if(n==1){
+            
+            return 1;
+        }
+        
+        int a = -1;
+        
+        if(dp[n]!= -1){
+            
+            return dp[n];
+        }
+        
+        for(int i = 1; i < n; i++){
+            
+            int b = i*(n-i);
+            int c = i*(find(n-i , dp));
+            
+            a = Math.max(a , Math.max(b , c));
+            
+        }
+        
+        
+      return dp[n] =  a;  
+        
+    }
+        
 }
